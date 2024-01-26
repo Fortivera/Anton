ef612fe289a8b9461b74e23584bec96702638d7b
Perchik <ant97str@gmail.com>
2024-01-25 19:11:47 -0500

    first commit

---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+export default function Blog(){
+    return (
+        <>
+            <header>
+                the blog page
+            </header>
+            <section>
+                story 1
+            </section>
+
+        </>
+    )
+}
---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <footer className="flex justify-center gap-4 h-14">
+            <div>Mail</div>
+            <div>Github</div>
+            <div>linked in</div>
+        </footer>
+    )
+}
